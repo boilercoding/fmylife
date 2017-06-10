69bf47dc19291f10890418ee5e8f364e497f7479
boilercoding <boilercoding@gmail.com>
2017-06-10 21:15:46 +0000

    Adds time ago to story timestamp template

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Fmylife.Mixfile do
   def application do
     [mod: {Fmylife, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :coherence, :timex]]
+                    :phoenix_ecto, :postgrex, :coherence]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,8 +38,7 @@ defmodule Fmylife.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:coherence, "~> 0.3"},
-     {:timex, "~> 3.0"}]
+     {:coherence, "~> 0.3"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
